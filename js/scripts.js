be79728be13c699773340378dce018b20f55d1e9
Reid Ashwill <reidashwill@gmail.com>
2020-04-01 16:18:09 -0700

    continue troubleshooting logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,32 @@
 //BACK END LOGIC
 
-var vowels = ["a", "e", "i", "o", "u"];
+output = []
 
-function firstLetter(string){
+function pigLatin(string){
   var firstLetter = string.charAt(0);
-  return first-letter;
-  console.log(first-letter)
-}
-
-function way(string){
-  if (first-letter === (/([vowels])/gi)){
-    return true;
-  }
-}
-
-function translate(string) {
-  if(way === true){
-
-    
-  }
-}
   
+  if(firstLetter.match(/[aeiou]/g)){
+    output.push(string + "way")
+  }
+
+
+  return output;
+}
+
+
+// function way(firstLetter){
+//   if (first-letter === /([vowels])/g){
+//     return true;
+//   }
+//   console.log(first-letter);
+// }
+
+// function translate(string) {
+//   if(way === true){
+
+//   }
+// }
+
 // UI LOGIC
 
 $(document).ready(function() {
@@ -29,8 +35,10 @@ $(document).ready(function() {
     $("result").hide()
     var string = $("#sentence").val();
     
+    var result = output.toString;
+
     
-    console.log(string)
+    
     $("#result").show()
   })
 });
